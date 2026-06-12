585743ac9999dc672b25f92a34aa346d1769109a
Stalin-21 <stalinguna21@gmail.com>
2026-06-12 12:21:25 +0530

    switched the gateway configuration yaml to java code for customization, commented out zipkin dependency for free run

---
+++ gateway/src/main/java/com/ecommerce/gateway/GatwayConfig.java
@@ -0,0 +1,34 @@
+package com.ecommerce.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatwayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("product-service",r -> r
+                        .path("/api/products/**")
+                        .uri("lb://PRODUCT-SERVICE"))
+
+                .route("user-service",r -> r
+                        .path("/api/users/**")
+                        .uri("lb://USER-SERVICE"))
+
+                .route("order-service",r -> r
+                        .path("/api/orders/**","/api/cart/**")
+                        .uri("lb://ORDER-SERVICE"))
+                .route("eureka-service",r -> r
+                        .path("/eureka/main")
+                        .filters(f -> f.setPath("/"))
+                        .uri("http://localhost:8761"))
+                .route("eureka-static-resources",r -> r
+                        .path("/eureka/**")
+                        .uri("http://localhost:8761"))
+                .build();
+    }
+}
